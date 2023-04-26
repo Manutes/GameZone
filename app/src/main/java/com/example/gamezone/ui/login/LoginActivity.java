02e16e184ac3c4e1435a8019c80b22c4435b1ee7
Manuel Vela <katernon@hotmail.es>
2023-04-26 18:39:51 +0200

    Make login screen ui

--- app/src/main/java/com/example/gamezone/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/gamezone/ui/login/LoginActivity.java
@@ -1,16 +1,28 @@
 package com.example.gamezone.ui.login;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.gamezone.databinding.ActivityLoginBinding;
 
-import com.example.gamezone.R;
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 }
