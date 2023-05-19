b9eb9fcdbfda354fa79006b19bb5897e3a504eda
Manuel Vela <katernon@hotmail.es>
2023-05-19 13:33:49 +0200

    Clean code

--- app/src/main/java/com/example/gamezone/ui/ranking/RankingFragment.java
+++ app/src/main/java/com/example/gamezone/ui/ranking/RankingFragment.java
@@ -2,21 +2,16 @@ package com.example.gamezone.ui.ranking;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.gamezone.R;
 import com.example.gamezone.data.models.GameRanking;
-import com.example.gamezone.databinding.FragmentOptionsBinding;
 import com.example.gamezone.databinding.FragmentRankingBinding;
-import com.example.gamezone.ui.games.GamesViewModel;
-import com.example.gamezone.ui.games.adapter.GamesAdapter;
 import com.example.gamezone.ui.ranking.adapter.RankingAdapter;
 
 import java.util.List;
--- app/src/main/java/com/example/gamezone/ui/ranking/adapter/RankingAdapter.java
+++ app/src/main/java/com/example/gamezone/ui/ranking/adapter/RankingAdapter.java
@@ -1,6 +1,5 @@
 package com.example.gamezone.ui.ranking.adapter;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
